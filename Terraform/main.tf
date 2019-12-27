4b36912c7fbec67dac8ddb7b25798ffa7fff3eb2
Fernando Celestino Paschualetto <fernando.paschualetto@matera.com>
2019-12-27 16:51:46 -0300

    Trabalhando com duas regiões

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -3,6 +3,12 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+provider "aws" {
+  alias = "us-east-2"
+  version = "~> 2.0"
+  region  = "us-east-2"
+}
+
 resource "aws_instance" "dev" {
   count = 3
   ami = "ami-04b9e92b5572fa0d1"
@@ -11,24 +17,65 @@ resource "aws_instance" "dev" {
   tags = {
       Name = "dev${count.index}"
   }
-  vpc_security_group_ids = ["sg-077a0fd3375ee836d"]
+  vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
 }
 
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
-  vpc_id      = "vpc-e24e6898"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    # Please restrict your ingress to only necessary IPs and ports.
-    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
-    cidr_blocks = ["179.184.65.86/32"]
+resource "aws_instance" "dev4" {
+  ami = "ami-04b9e92b5572fa0d1"
+  instance_type = "t2.micro"
+  key_name = "terraform-aws"
+  tags = {
+      Name = "dev4"
   }
+  vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
+  depends_on = [aws_s3_bucket.dev4]
+}
+
+resource "aws_instance" "dev5" {
+  ami = "ami-04b9e92b5572fa0d1"
+  instance_type = "t2.micro"
+  key_name = "terraform-aws"
+  tags = {
+      Name = "dev5"
+  }
+  vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
+}
+
+resource "aws_instance" "dev6" {
+  provider = aws.us-east-2
+  ami = "ami-0dacb0c129b49f529"
+  instance_type = "t2.micro"
+  key_name = "terraform-aws"
+  tags = {
+      Name = "dev6"
+  }
+  vpc_security_group_ids = ["${aws_security_group.allow_ssh-us-east-2.id}"]
+  depends_on = [aws_dynamodb_table.dynamodb-homolog]
+}
+
+resource "aws_s3_bucket" "dev4" {
+  bucket = "paschualetto-dev4"
+  acl    = "private"
 
   tags = {
-    Name = "allow_ssh"
+    Name = "paschualetto-dev4"
+  }
+}
+
+resource "aws_dynamodb_table" "dynamodb-homolog" {
+  provider       = aws.us-east-2
+  name           = "UserData"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "UserId"
+  range_key      = "UserEmail"
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+
+  attribute {
+    name = "UserEmail"
+    type = "S"
   }
 }
---
+++ Terraform/security-group.tf
@@ -0,0 +1,36 @@
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = ["179.184.65.86/32"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
+resource "aws_security_group" "allow_ssh-us-east-2" {
+  provider    = aws.us-east-2
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = ["179.184.65.86/32"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
